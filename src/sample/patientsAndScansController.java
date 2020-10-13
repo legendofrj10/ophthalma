c3a6c34731767a0140bf0b46822091f8822a0297
yashraj jangir <yashraj.jangir69@gmail.com>
2020-10-13 21:28:30 +0530

    progress

---
+++ src/sample/patientsAndScansController.java
@@ -0,0 +1,72 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class patientsAndScansController {
+    @FXML
+    private Button registerPatientBTN;
+
+    @FXML
+    private Button helpAndFaqBTN,patientsAndScansBTN,dashboardBTN,logOutBTN,settingsBTN,notificationBTN,chatBubbleBTN;
+
+    @FXML
+    void callChatBubble() {
+
+    }
+
+    @FXML
+    void callDashboard() throws IOException {
+        Stage stage = (Stage) dashboardBTN.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    void callHelpAndFaq() throws IOException {
+        Stage stage = (Stage) helpAndFaqBTN.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("helpAndFaq.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    void callLogOut() {
+
+    }
+
+    @FXML
+    void callNotifications() {
+
+    }
+
+    @FXML
+    void callPatientsAndScans() throws IOException {
+        Stage stage = (Stage) patientsAndScansBTN.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("patientsAndScans.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+    }
+
+    @FXML
+    void callSettings() {
+
+    }
+
+    @FXML
+    void callRegisterPatient() throws IOException {
+        Stage stage = (Stage) registerPatientBTN.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("patientsRegister.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+}
---
+++ src/sample/patientsRegisterController.java
@@ -0,0 +1,48 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class patientsRegisterController {
+
+    @FXML
+    private TextField patientNameField;
+
+    @FXML
+    private ComboBox<?> patientGenderCB;
+
+    @FXML
+    private DatePicker patientDOB;
+
+    @FXML
+    private ComboBox<?> patientEyeCB;
+
+    @FXML
+    private Button registerBTN;
+
+    @FXML
+    private Button backBTN;
+
+    @FXML
+    void callBack() throws IOException {
+        Stage stage = (Stage) backBTN.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("patientsAndScans.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    void callRegister() {
+
+    }
+
+}
